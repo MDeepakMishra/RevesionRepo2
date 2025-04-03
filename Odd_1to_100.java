67fb410a04efe9a29d6647fceb257a17854fd1d2
deep misra <deepakmishra93152@gmail.com>
2025-04-03 17:31:46 +0530

    Print Odd numbr from 1 to 100

---
+++ Odd_1to_100.java
@@ -0,0 +1,11 @@
+public class Odd_1to_100 {
+    public static void main(String[] args) {
+        System.out.println("PRINT ODD NUM FROM 1 TO 100");
+        int n = 100;
+        for( int i = 1 ; i < n ; i++){
+            if( i % 2 != 0){
+                System.out.print( i + " ");
+            }
+        }
+      }
+}
