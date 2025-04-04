1ef2c48e0cbef08c322ea7a96f42bffe4286f25d
deep misra <deepakmishra93152@gmail.com>
2025-04-04 10:53:41 +0530

    Swap using three veriables

---
+++ Swap1.java
@@ -0,0 +1,13 @@
+//System.out.println("Swap usng three veriables");
+class Swap1 {
+    public static void main(String[] args) {
+        int a = 4 , b = 7 , c ;
+        System.out.println("Before Swaping number" );
+        System.out.println(a +" " + b);
+        System.out.println("After Swaping Number");
+        c = a ;
+        a = b ;
+        b = c ;
+        System.out.println(a +" " + b);
+    }
+}
