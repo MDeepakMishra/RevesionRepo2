036e2e19b948c6cee25e218e6d604f0a5b40dbcf
deep misra <deepakmishra93152@gmail.com>
2025-04-07 19:40:20 +0530

    Second way to print any string

---
+++ String_Print.java
@@ -0,0 +1,7 @@
+// Print string by string literal
+public class String_Print {
+  public static void main(String[] args) {
+        String s = "Deepak Mishra";
+        System.out.println(s);
+    }
+}
