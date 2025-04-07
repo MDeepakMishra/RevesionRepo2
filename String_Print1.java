f101f092275dbf976b8182645115dc320c7a295a
deep misra <deepakmishra93152@gmail.com>
2025-04-07 19:35:47 +0530

    print  the string

---
+++ String_Print1.java
@@ -0,0 +1,7 @@
+// Print string by new keyboard
+public class String_Print1 {
+public static void main(String[] args) {
+    String s = new String("Deepak Kumar");
+    System.out.println(s);
+}    
+}
